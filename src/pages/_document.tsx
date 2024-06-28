a1dba9e4c0eae81a7dfd1c1ec2bb0f0c248959b7
Daniel24125 <danielmadalena@msn.com>
2024-06-28 14:48:39 +0100

    adding login button to page

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,16 @@
 import { Html, Head, Main, NextScript } from "next/document";
-
+import { UserProvider } from '@auth0/nextjs-auth0/client';
+ 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
+      <UserProvider>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </UserProvider>
     </Html>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <main>
-      
+      <a href="/api/auth/login">Login</a>
     </main>
   );
 }
