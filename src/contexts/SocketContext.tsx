7b7c2b4b730eaecad30440dba083e6f1b0aab191
Daniel24125 <danielmadalena@msn.com>
2024-07-30 15:23:11 +0100

    trying to implmente socketio client

--- src/contexts/SocketContext.tsx
+++ src/contexts/SocketContext.tsx
@@ -1,6 +1,9 @@
 import { useEventListener } from '@/lib/customHooks';
 import { SocketOptionsSchema } from '@/types/Socket';
 import React from 'react'
+import { io } from 'socket.io-client';
+
+
 
 export const SocketContextProvider = React.createContext<{
     socketOptions: SocketOptionsSchema | null,
@@ -19,29 +22,22 @@ const SocketContext = ({children}: {children: React.ReactNode}) => {
    
     
     React.useEffect(()=>{
-        const socket = new WebSocket(process.env.NEXT_PUBLIC_BACKEND_API_URL as string);
-
-        socket.addEventListener("open", ()=>{
-            console.log("Socket connected!")
-            setSocketOptions({
-                //@ts-ignore
-                socket: socket
-            })
-            socket.send(JSON.stringify({
-                "cmd": "nir_status", 
-                "data": "HELLO"
-            }))
+        const socket = io(process.env.NEXT_PUBLIC_BACKEND_API_URL as string, { transports: ['websocket'], autoConnect: false });
+        setSocketOptions({
+            //@ts-ignore
+            socket
         })
-
-        socket.addEventListener("message", (data: any)=>{
-            console.log("Message Received: ", data)
+        socket.on("connect", ()=>{
+            console.log("Connected to the server!")
+        })
+        
+        socket.on("connect_error", (err) => {
+            console.log(`connect_error due to ${err.message}`);
             
-        })
-
-        socket.addEventListener("close", (data: any)=>{
-            console.log("Connection closed!")
-        })
-       
+          });
+        socket.on('disconnect', ()=>{
+            console.log("Disconnected to the server!")
+        });
     },[])
     return (<SocketContextProvider.Provider value={{socketOptions, setSocketOptions}}>
         {children}
@@ -51,3 +47,28 @@ const SocketContext = ({children}: {children: React.ReactNode}) => {
 
 
 export default SocketContext
+
+
+
+// const socket = new WebSocket(process.env.NEXT_PUBLIC_BACKEND_API_URL as string);
+
+//         socket.addEventListener("open", ()=>{
+//             console.log("Socket connected!")
+//             setSocketOptions({
+//                 //@ts-ignore
+//                 socket: socket
+//             })
+//             socket.send(JSON.stringify({
+//                 "cmd": "nir_status", 
+//                 "data": "user"
+//             }))
+//         })
+
+//         socket.addEventListener("message", (data: any)=>{
+//             console.log("Message Received: ", data)
+            
+//         })
+
+//         socket.addEventListener("close", (data: any)=>{
+//             console.log("Connection closed!")
+//         })
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import { Button } from "@/components/ui/button";
 import { PageContextProvider, TPageParamsSchema } from "@/contexts/PageContext";
 import { useRouter } from "next/router";
+import { SocketContextProvider } from "@/contexts/SocketContext";
 
 
 
@@ -9,18 +10,22 @@ export default function Home() {
   const router = useRouter()
   //@ts-ignore
   const {updatePageParams} = React.useContext(PageContextProvider)
-
+  //@ts-ignore
+  const {socketOptions} = React.useContext(SocketContextProvider);
 
   React.useEffect(()=>{
     updatePageParams({title: "Dashboard"})
+    
   },[])
 
   return (
     <div>
       AUTHENTICATED
       <Button onClick={()=>{
-        router.push("/api/auth/logout")
-      }}>Logout</Button>
+        if(socketOptions.socket) {
+          socketOptions.socket.connect()
+        }
+      }}>Connect to socket</Button>
     </div>
   );
 }
